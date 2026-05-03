658113bd537c1fe343dcb13b0833beb9c6a76b23
Sebantonio <sebantonio@gmail.com>
2026-05-03 16:08:23 +0200

    fix: icono 🔁 uniforme en tarjetas y tabla

--- js/inventory.js
+++ js/inventory.js
@@ -106,7 +106,7 @@ function rCards(data,mc){
         <button class="btn btn-sm" onclick="openModal(${x.id})" title="Editar">✏️</button>
         <button class="btn btn-sm" onclick="duplicateItem(${x.id})" title="Duplicar">⧉</button>
         <button class="btn btn-sm" onclick="openDocsModal(${x.id})" title="Documentación">📌</button>
-        <button class="btn btn-sm btn-loan" onclick="openPresDevModal(${x.id})" title="Prestar / Devolver"><img src="icons/prestar.png" style="width:16px;height:16px;vertical-align:middle"></button>
+        <button class="btn btn-sm btn-loan" onclick="openPresDevModal(${x.id})" title="Prestar / Devolver" style="font-size:16px;line-height:1">🔁</button>
         <button class="btn btn-sm btn-pedido${isPedido(x.id)?' activo':''}" onclick="togglePedido(${x.id})" title="Pedido">🛒</button>
         ${x.est!=='Baja'?`<button class="btn btn-sm btn-baja" onclick="openBaja(${x.id})">⛔</button>`:''}
         <button class="btn btn-sm btn-d" onclick="confDel(${x.id})" title="Eliminar">🗑</button>
