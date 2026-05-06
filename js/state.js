ae9138ce4623aea597c65263b7fcadc4660edc68
Sebantonio <sebantonio@gmail.com>
2026-05-06 20:45:09 +0200

    vuela a atras appscript

--- js/state.js
+++ js/state.js
@@ -1,7 +1,7 @@
 // ═════════════════════════════════════════════════════════
 // ESTADO
 // ═════════════════════════════════════════════════════════
-let API_URL = 'https://script.google.com/macros/s/AKfycby0yAXCBztS8wnlSqP1WomwgSDHf3n-YySsuSnh-FYV5LfpmMIxEYVlTsPyW4Oxx-F_7g/exec';
+let API_URL = 'https://script.google.com/macros/s/AKfycbywyDQBOm0cwR6ILV7V0Wj1G0cD9Lw3RZjKWi4ZP1uczeXhdr0cp4a_uIDzj6JrzWtY/exec';
 let SESSION = JSON.parse(localStorage.getItem('inv_session') || 'null');
 let items = [];
 let profesores = [];
